70fb3c7c9140e52eaa768b0a0c7c18d3a1edf007
SalimYami <cephussalim@gmail.com>
2025-01-19 12:28:28 +0100

    Je dois tester les methodes d'evaluation controller

--- Projet_Si-master/Conference/src/main/java/tpsi2/Conference/Controller/EvaluationController.java
+++ Projet_Si-master/Conference/src/main/java/tpsi2/Conference/Controller/EvaluationController.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import tpsi2.Conference.Enumeration.EtatEvaluation;
 import tpsi2.Conference.Model.EvaluationModele;
+import tpsi2.Conference.Model.SoumissionModele;
 import tpsi2.Conference.Service.EvaluationService;
 import tpsi2.Conference.Service.UtilisateurService;
 
@@ -26,6 +28,7 @@ public class EvaluationController {
         return ResponseEntity.ok(evaluationService.createEvaluation(soumissionId, evaluationModele));
 
     }
+    //A TESTER POUR SALIM
 //ajout d'evaluation consulter Merwane demain + Essayer de faire une meilleur implementation
     @PostMapping ("{conferenceID}/evaluations")
     @PreAuthorize("@utilisateurService.hasRoleInConference(#conferenceID,'EDITEUR')")
@@ -36,4 +39,41 @@ public class EvaluationController {
         return ResponseEntity.ok(evaluationService.findEvaluationByConferenceId(conferenceID));
 
     }
+ //Un evaluateur peut modifier ses evaluations
+ //un editeur peut supprimer des evaluations
+ //un editeur peut changer l'etat de l'evaluation
+
+    @PutMapping("{conferenceID}/{evaluationID}/modify")
+    @PreAuthorize("@utilisateurService.hasThisRole('EVALUATEUR')")
+    public ResponseEntity<?> modifiyEvaluation(
+            @PathVariable("conferenceID") Long conferenceID,
+            @PathVariable("evaluationID") Long evaluationID,
+            @RequestBody EvaluationModele evaluationModele
+    ) {
+        return ResponseEntity.ok(evaluationService.modifyEvaluation(conferenceID,evaluationID,evaluationModele.getCommentaire(),evaluationModele.getNote()));
+
+    }
+
+    @PutMapping("{conferenceID}/{evaluationID}/modifyState/{etatEvaluation}")
+    @PreAuthorize("@utilisateurService.hasRoleInConference(#conferenceID,'EDITEUR')")
+    public ResponseEntity<?> modifiyEtatEvaluation(
+            @PathVariable("conferenceID") Long conferenceID,
+            @PathVariable("evaluationID") Long evaluationID,
+            @PathVariable EtatEvaluation etatEvaluation
+    ) {
+        return ResponseEntity.ok(evaluationService.modifyEtatEvaluation(conferenceID,evaluationID,etatEvaluation));
+
+    }
+
+    @PutMapping("{conferenceID}/{evaluationID}/delete")
+    @PreAuthorize("@utilisateurService.hasRoleInConference(#conferenceID,'EDITEUR')")
+    public ResponseEntity<?> supprimerEvaluation(
+            @PathVariable("conferenceID") Long conferenceID,
+            @PathVariable("evaluationID") Long evaluationID
+    ) {
+        return ResponseEntity.ok(evaluationService.deleteEvaluation(conferenceID,evaluationID));
+
+    }
+
+
 }
--- Projet_Si-master/Conference/src/main/java/tpsi2/Conference/Service/EvaluationService.java
+++ Projet_Si-master/Conference/src/main/java/tpsi2/Conference/Service/EvaluationService.java
@@ -1,10 +1,13 @@
 package tpsi2.Conference.Service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import tpsi2.Conference.Entities.*;
+import tpsi2.Conference.Enumeration.EtatConference;
 import tpsi2.Conference.Enumeration.EtatEvaluation;
+import tpsi2.Conference.Enumeration.Note;
 import tpsi2.Conference.Model.EvaluationModele;
 import tpsi2.Conference.Repositories.*;
 
@@ -60,6 +63,84 @@ public class EvaluationService {
         return (List<Evaluation>) evaluationRepository.findAllByConference(conference.orElse(null));
     }
 
+    //modifier Evaluation
+    public ResponseEntity<?> modifyEvaluation(Long conferenceId,Long evaluationID, String newComm, Note newNote) {
+
+        Conference conference = conferenceRepository.findById(conferenceId).orElse(null);
+        if (conference != null) {
+
+                Optional<Utilisateur> utilisateur = utilisateurService.getUserFromContext();
+                //on fait ca pour recuperer qui envoie la demande
+                Evaluation evaluation = evaluationRepository.findById(evaluationID).orElse(null);
+
+                if (evaluation != null) {
+                    if (conference.getEtat().equals(EtatConference.OUVERTE)) {
+                        if(evaluation.getEvaluateur().equals(utilisateur)){
+                    evaluation.setCommentaire(newComm);
+                    evaluation.setNote(newNote);
+                    evaluationRepository.save(evaluation);
+                    return ResponseEntity.ok().body("Evaluation modifiée !");
+                        }
+                        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                                .body("Vous n'etes pas autorisé ");
+                    }
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.badRequest().body("Conference fermée");
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    //modifier etat evaluation pour l'Editeur
+    public ResponseEntity<?> modifyEtatEvaluation( Long conferenceId,Long evaluationID,EtatEvaluation etatEvaluation) {
+        Conference conference = conferenceRepository.findById(conferenceId).orElse(null);
+
+        if (conference != null) {
+            Evaluation evaluation = evaluationRepository.findById(evaluationID).orElse(null);
+            if (evaluation != null) {
+
+                if (conference.getEtat().equals(EtatConference.OUVERTE)) {
+                    evaluation.setEtatEvaluation(etatEvaluation);
+                    evaluationRepository.save(evaluation);
+                    return ResponseEntity.ok().body("Etat d'évaluation modifiée !");
+                }
+
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.badRequest().body("Conference fermée");
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+
+    //Supprimer evaluation
+    public ResponseEntity<?> deleteEvaluation( Long conferenceId,Long evaluationID) {
+        Conference conference = conferenceRepository.findById(conferenceId).orElse(null);
+
+        if (conference != null) {
+            Evaluation evaluation = evaluationRepository.findById(evaluationID).orElse(null);
+            if (evaluation != null) {
+
+                if (conference.getEtat().equals(EtatConference.OUVERTE)) {
+                    evaluationRepository.delete(evaluation);
+                    return ResponseEntity.ok().body("Supprimée");
+                }
+
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.badRequest().body("Conference fermée");
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
 
 
 
